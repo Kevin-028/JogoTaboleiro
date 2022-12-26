a9d4883b180c9255b8f0110fd3cf44d42758f013
Kevin <kevinromeiro@gmail.com>
2022-12-26 20:01:12 -0300

    123

--- JogoTabuleiro/Program.cs
+++ JogoTabuleiro/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
 
-var teste = "dasdas"
+var teste = "dasdas";
+
+var teste2 = "dasda";
